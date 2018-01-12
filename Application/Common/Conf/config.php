55734d43371280a65994a55c5cb8ba9b6b99b717
joylee109 <1515885616@qq.com>
2018-01-12 14:23:01 +0800

    first demo layuicms thinkphp3.2.3

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'MODULE_DENY_LIST'=>array('Common','Api'),
+    'MODULE_ALLOW_LIST'=>array('Home','Admin'),
+    'DEFAULT_MODULE' => 'Home',
+    'DEFAULT_CONTROLLER' =>'Index',
+    'DEFAULT_ACTION' =>'index',
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'localhost',
+    'DB_NAME'=>'thinkphp',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'root123',
+    'DB_PORT'=>3306,
+    'DB_PREFIX'=>'db_'
+);
