d23fe054ac3453625f13567af3b7aea279bd936c
joylee109 <1515885616@qq.com>
2018-01-17 13:44:51 +0800

    link controller

---
+++ Application/Admin/Controller/LinkController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AdminbaseController;
+class LinkController extends AdminbaseController {
+    public function index(){
+        echo 'asdfasdf';
+    }
+}
